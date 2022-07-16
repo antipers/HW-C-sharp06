8b996e07de4eb33137019c37ae2aeb19a0d7c63c
antipers <antipers@gmail.com>
2022-07-16 23:38:05 +0300

    solved 43 =/

--- task43/Program.cs
+++ task43/Program.cs
@@ -3,11 +3,25 @@
 // значения b1, k1, b2 и k2 задаются пользователем
 
 System.Console.WriteLine("Введите значение b1");
-int b1= int.Parse(Console.ReadLine());
+double b1 = double.Parse(Console.ReadLine());
 System.Console.WriteLine("Введите значение k1");
-int k1= int.Parse(Console.ReadLine());
+double k1 = double.Parse(Console.ReadLine());
 System.Console.WriteLine("Введите значение b2");
-int b2= int.Parse(Console.ReadLine());
+double b2 = double.Parse(Console.ReadLine());
 System.Console.WriteLine("Введите значение k2");
-int k2= int.Parse(Console.ReadLine());
+double k2 = double.Parse(Console.ReadLine());
 
+Checking(b1, k1, b2, k2);
+void Checking(double b1, double k1, double b2, double k2)
+{
+    if (k1 - k2 == 0)
+    {
+        Console.WriteLine("Заданные прямые параллельны");
+    }
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Найдено пересечение в точке: ({x};{y})");
+    }
+}

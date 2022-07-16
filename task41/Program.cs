4e153607868a735cba01fab2aa5863e31a3c34d6
antipers <antipers@gmail.com>
2022-07-16 23:04:37 +0300

    solved task41

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Сколько чисел вы хотите ввести?");
+int Num= int.Parse(Console.ReadLine()!);
+
+int[] array= new int[Num];
+int count=0;
+for (int i = 0; i < array.Length; i++)
+{
+
+    array[i] = int.Parse(Console.ReadLine()!);
+    if (array[i]>0) count++;
+    
+}
+System.Console.WriteLine($"Число положительных = {count}");
---
+++ task43/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем
+
+System.Console.WriteLine("Введите значение b1");
+int b1= int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите значение k1");
+int k1= int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите значение b2");
+int b2= int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите значение k2");
+int k2= int.Parse(Console.ReadLine());
+
